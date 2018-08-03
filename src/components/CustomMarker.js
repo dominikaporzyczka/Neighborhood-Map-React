280dde9572a9a6bbb10d5b7ebf49f75189b7f1da
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2018-08-03 23:38:17 +0200

    Add onClick func

--- src/components/CustomMarker.js
+++ src/components/CustomMarker.js
@@ -2,14 +2,33 @@ import React, { Component } from 'react';
 import { Marker, InfoWindow } from 'react-google-maps';
 
 class CustomMarker extends Component {
+    state = {
+        isOpen: false
+    }
+
+    onClickMarker = () => {
+        const self = this
+
+        if (self.state.isOpen && self.props.location.name === self.props.currentMarker) {
+            self.props.setCurrentMarker('')
+        } else if (!self.state.isOpen) {
+            self.props.setCurrentMarker(self.props.location.name)
+        }
+
+        self.setState((prevState) => ({
+            isOpen: !prevState.isOpen
+        }))
+    }
+
     render() {
         return (
             <Marker
                 position={{ lat: this.props.location.lat, lng: this.props.location.lng }}
+                onClick={this.onClickMarker}
             >
-                <InfoWindow>
+                {this.state.isOpen && <InfoWindow>
                     <div>Hi, I'm InfoWindow</div>
-                </InfoWindow>
+                </InfoWindow>}
             </Marker>
         );
     }
