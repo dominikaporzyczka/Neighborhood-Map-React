8e4bfa5b3d4fdea4a20040bcd7ff98fdf8521aa8
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2018-08-04 22:43:15 +0200

    Add infowindow content

--- src/components/InfoAboutLocation.js
+++ src/components/InfoAboutLocation.js
@@ -16,13 +16,13 @@ class InfoAboutLocation extends Component {
         const { data } = this.state
 
         return (
-            <div>
+            <div className="infowindow">
                 {!data && <div>Loading...</div>}
                 {data && 
                     <div>
-                        <p>{data.name}</p>
-                        <p>{data.categories[0].name}</p>
-                        <p>{data.location.formattedAddress.join(', ')}</p>
+                        <p className="infowindow-name">{data.name}</p>
+                        <p className="infowindow-type">{data.categories[0].name}</p>
+                        <p className="infowindow-address">{data.location.formattedAddress.join(', ')}</p>
                     </div>
                 }
 
