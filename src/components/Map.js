47ff972139fc3d96fbc1ba704d5b8025084903ba
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2018-08-04 19:51:41 +0200

    Remove import css

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap } from 'react-google-maps';
-import '../App.css';
 import CustomMarker from './CustomMarker';
 
 const Map = withScriptjs(withGoogleMap(props => (
