a775dbdcebe8afcb21c28f965f0eb8fe9f621417
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2018-08-06 10:56:59 +0200

    Move data about markers to separate file

---
+++ src/MarkersData.js
@@ -0,0 +1,29 @@
+export default {
+    data: [
+        {
+            name: 'Polish Lody',
+            lat: 51.1165598,
+            lng: 17.042102
+        },
+        {
+            name: 'Rynek',
+            lat: 51.1094543,
+            lng: 17.03133219999995
+        },
+        {
+            name: 'ZOO Wrocław',
+            lat: 51.1041258,
+            lng: 17.074197000000026
+        },
+        {
+            name: 'Przełam Lody',
+            lat: 51.1083577,
+            lng: 17.068400999999994
+        },
+        {
+            name: 'Park Zachodni',
+            lat: 51.1323534,
+            lng: 16.969623400000046
+        }
+    ]
+}
--- src/components/App.js
+++ src/components/App.js
@@ -2,38 +2,13 @@ import React, { Component } from 'react';
 import Map from './Map'
 import '../App.css';
 import Search from './Search';
+import MarkersData from '../MarkersData'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      allLocations: [
-        {
-          name: 'Polish Lody',
-          lat: 51.1165598,
-          lng: 17.042102
-        },
-        {
-          name: 'Rynek',
-          lat: 51.1094543,
-          lng: 17.03133219999995
-        },
-        {
-          name: 'ZOO Wrocław',
-          lat: 51.1041258,
-          lng: 17.074197000000026
-        },
-        {
-          name: 'Przełam Lody',
-          lat: 51.1083577,
-          lng: 17.068400999999994
-        },
-        {
-          name: 'Park Zachodni',
-          lat: 51.1323534,
-          lng: 16.969623400000046
-        },
-      ],
+      allLocations: MarkersData.data,
       currentMarker: '',
       filteredLocations: [],
       showSideBar: true
